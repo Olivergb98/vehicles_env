d94139a50bad5b41dbd9aa253eb17fd99384d042
Olivergb98 <olivergb98@gmail.com>
2024-11-06 13:06:32 -0600

    Se corrigió el formato de los nobmres de columnas y se eliminó la opción del botón interactivo.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # vehicles_env
 Proyecto de sitio web sobre ventas de vehículos
 
-El sitio web debe de contar con un botón para construir un histograma del odómetro y el precio de los vehículos, o, en su defecto, una checkbox que habilite la creación de dicho histograma.
+El sitio web debe de contar con un checkbox para construir un histograma del odómetro y el precio de los vehículos en el dataframe proporcionado.
--- app.py
+++ app.py
@@ -2,20 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-car_data = pd.read_csv('vehicles_us.csv')
-
-hist_button = st.button('Construir histograma')  # crear un botón
-if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
-    st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
-
+car_data = pd.read_csv('vehicles_us.csv')  # importar df
+car_data.columns = car_data.columns.str.strip()  # limpiar nombres de columnas
 build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram:  # si la casilla de verificación está seleccionada
